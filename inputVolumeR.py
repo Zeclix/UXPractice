3b794ed491e6a8b4d5d56fa3567c330a3d00f7e2
Zeclix <zeclix@yonsei.ac.kr>
2016-09-26 17:23:29 +0900

    .

--- inputVolumeR.py
+++
@@ -1,8 +0,0 @@
-width = float(input("Enter width : "))
-height = float(input("Enter height: "))
-depth = float(input("Enter depth: "))
-
-volume = width*height*depth
-
-
-print("Volume : %f" % volume)
