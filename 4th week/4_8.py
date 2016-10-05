a39a364be1422f94a85dc047d83ed8bab0c475f1
Zeclix <zeclix@yonsei.ac.kr>
2016-10-05 13:33:47 +0900

    4th week

---
+++ 4th week/4_8.py
@@ -0,0 +1,7 @@
+pName = input("Enter the value of pName : ")
+pTime = input("Enter the value of pTime : ")
+pPriority = input("Enter the value of pPriority : ")
+
+dic = {'pName' : pName, 'pTime': pTime, 'pPriority':pPriority}
+
+print(dic.items())
