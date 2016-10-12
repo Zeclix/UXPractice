7651af03634a681515f423e1ef1b9a32dc53f86f
Zeclix <zeclix@yonsei.ac.kr>
2016-10-12 20:40:16 +0900

    6th week

--- 5th week/5_2.py
+++ 5th week/5_2.py
@@ -3,7 +3,7 @@ SUM = 0
 cnt = 0
 a=[]
 while cnt<10:
-   temp = int(input("%d번째 값을 입력하세요:" %(cnt+1)))
+   temp = float(input("%d번째 값을 입력하세요:" %(cnt+1)))
    a.append(temp)
    cnt = cnt+1
 
@@ -11,14 +11,15 @@ MAX = a[0]
 for i in a:
     if i>MAX:
         MAX = i
-print("최대값 : %d" % MAX)
+print("최대값 : %f" % MAX)
 
 MIN = a[0]
 for i in a:
     if i<MIN:
         MIN = i
-print("최소값 : %d" % a[0])
+print("최소값 : %f" % a[0])
 for i in a:
     SUM = SUM + i
-print("합 : %d" % SUM)
+print("합 : %f" % SUM)
+print("평균 : %f" % (SUM/len(a)))
 
